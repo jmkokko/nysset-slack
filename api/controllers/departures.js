a5da794b9cf857e9bb69542df1b32b2575e61b5f
Janne <jmkokko1@gmail.com>
2016-12-03 16:59:49 +0200

    Experimenting with output format

--- api/controllers/departures.js
+++ api/controllers/departures.js
@@ -1,18 +1,32 @@
 "use strict";
 
+const debug = require("debug")("nysset-slack");
+
 function departures(req, res) {
 
     // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
     const name = req.swagger.params.name.value;
     let response;
 
+    debug(`name: ${name}`);
+
     response = {
         "response_type": "in_channel",
-        "text": "Fredrikinkatu 0804: http://aikataulut.hsl.fi/pysakit/fi/1040438.html",
+        "text": "*0804 Fredrikinkatu*    http://aikataulut.hsl.fi/pysakit/fi/1040438.html",
         "attachments": [
             {
-                // "title": "App hangs on reboot",
-                "text": "*TRAM 3* Urheilutalo `23:19  01:12  01:32`",
+                "color": "#36a64f",
+                "author_name": "3  Olympiaterminaali-Eira-Kallio-Nordenskiöldinkatu",
+                "author_link": "https://linjakartta.reittiopas.fi/fi/#?mapview=map&line=1003++1,1003++2",
+                "author_icon": "https://linjakartta.reittiopas.fi/img/tram.png",
+                "title": "23:19  01:12  01:32",
+            },
+            {
+                "color": "#36a64f",
+                "author_name": "6T  Länsiterminaali-Arabia",
+                "author_link": "https://linjakartta.reittiopas.fi/fi/#?mapview=map&line=1006T+1,1006T+2",
+                "author_icon": "https://linjakartta.reittiopas.fi/img/bus.png",
+                "title": "23:19  01:12  01:32",
             }
         ]
     };
@@ -24,3 +38,31 @@ function departures(req, res) {
 module.exports = {
     departures
 };
+
+// {
+//     "attachments": [
+//     {
+//         "fallback": "Required plain-text summary of the attachment.",
+//         "color": "#36a64f",
+//         "pretext": "Optional text that appears above the attachment block",
+//         "author_name": "Bobby Tables",
+//         "author_link": "http://flickr.com/bobby/",
+//         "author_icon": "http://flickr.com/icons/bobby.jpg",
+//         "title": "Slack API Documentation",
+//         "title_link": "https://api.slack.com/",
+//         "text": "Optional text that appears within the attachment",
+//         "fields": [
+//             {
+//                 "title": "Priority",
+//                 "value": "High",
+//                 "short": false
+//             }
+//         ],
+//         "image_url": "http://my-website.com/path/to/image.jpg",
+//         "thumb_url": "http://example.com/path/to/thumb.png",
+//         "footer": "Slack API",
+//         "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+//         "ts": 123456789
+//     }
+// ]
+// }
